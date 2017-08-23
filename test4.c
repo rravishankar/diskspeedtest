ea4332c6521b5d8df61c78969767ce74c368e9e8
Ravishankar Rajagopalan <rravi108@gmail.com>
2017-08-23 11:04:27 +0000

    Cleaned up some messages

--- test4.c
+++ test4.c
@@ -12,72 +12,66 @@
 #define KILOBYTE (1024)
 
 /* Takes two arguments.  Arg 1 is file name.
-arg 2 is number of megabytes to write*/
+arg 2 is number of megabytes to write, arg 3 is the block size in k */
 main(int argc, char * argv[]) {
-	long megabytes, size = 64*1024; //size in k;
-	char *filename;
-	int i = 0;
+        long megabytes, size = 64 * KILOBYTE; //size in k;
+        char * filename;
 
-	if (argc !=3)
-	    {
-	    printf("Arguments are filename to create and size in megabytes\n");
-	    return -1;
-	    }
+        if (argc != 3) {
+                printf("Arguments are filename to create and size in megabytes\n");
+                return -1;
+        }
 
-	megabytes=atol(argv[2]);
-	filename = argv[1];
-	while (size <= 16*1024*1024) {//till 16M
-		writer(argc, megabytes, size, filename);
-		size = size * 2;
-	}
+        megabytes = atol(argv[2]);
+        filename = argv[1];
+        while (size <= 16 * MEGABYTE) { //till 16M
+                writer(argc, megabytes, size, filename);
+                size = size * 2;
+        }
 
 }
 
-writer (int argc, long megabytes, long writeblocksize, char *filename)
-{
-	long j,i,r;
-	int outputfile;
-	char *buffer;
-	clock_t t;
+writer(int argc, long megabytes, long writeblocksize, char * filename) {
+        long i, r;
+        int outputfile;
+        char * buffer;
+        clock_t t;
 
+        //printf("Blocksize= %ldk\n", writeblocksize/1024);
 
-	
+        buffer = malloc(writeblocksize);
 
-	
-	//printf("Blocksize= %ldk\n", writeblocksize/1024);
+        if (buffer == NULL) {
+                printf("Out of memory\n");
+                exit(1);
+        }
 
-	buffer = malloc(writeblocksize);
+        for (i = 0; i < writeblocksize; i++)
+                buffer[i] = 'X';
 
-	if (buffer == NULL) {
-		printf("Out of memory\n");
-		exit (1);
-	}
+        outputfile = open(filename, O_CREAT | O_WRONLY | O_SYNC);
 
-	for (j=0 ; j < writeblocksize ;j++)
-	    buffer[j]='X';
+        t = clock();
+        //printf("Megabytes %ld, writeblocksize %ld\n", megabytes, writeblocksize);
+        r = (megabytes * MEGABYTE) / writeblocksize;
+        printf("Writing block size of %ld, %ld times, file size %ldMB\n",writeblocksize, r, megabytes);
+        for (i = 0; i < ((megabytes * MEGABYTE / writeblocksize)); i++)
+                write(outputfile, buffer, writeblocksize);
 
-	outputfile = open(filename, O_CREAT | O_WRONLY | O_SYNC);
+        t = clock() - t;
+        double time_taken = ((double) t) / CLOCKS_PER_SEC; // in seconds
 
+        //printf("Time taken %lf\n", time_taken);
+        if (time_taken > 0) {
+                //printf("Bytes per second written = %lf\n", (MEGABYTE * megabytes) / time_taken);
+                double mbps = 1.0 * megabytes / time_taken;
+                //printf("Megabytes = %ld, timetowrite seconds %lf\n",megabytes, time_taken);
+                printf("Write speed in megabytes per second = %4.2lf, Blocksize:%ldk\n", mbps, writeblocksize / KILOBYTE);
+        } else {
+                printf("Something wrong, time taken to write should be greater than 0!\n");
+        }
 
-	t = clock();
-	//printf("Megabytes %ld, writeblocksize %ld\n", megabytes, writeblocksize);
-	r = (megabytes*MEGABYTE)/writeblocksize;
-	//printf("Writing block size of %ld, %ld times\n",writeblocksize, r);
-	for (i=0 ; i<((megabytes*MEGABYTE/writeblocksize)) ; i++)
-	   write(outputfile,buffer,writeblocksize);
+        close(outputfile);
 
-	t = clock() -t;
-	double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
-
-	//printf("Time taken %lf\n", time_taken);
-	if (time_taken > 0) {
-		printf("Bytes per second written = %lf\n",(MEGABYTE*megabytes)/time_taken);
-	 	double mbps = 1.0 * megabytes/time_taken;
-	 	//printf("Megabytes = %ld, timetowrite seconds %lf\n",megabytes, time_taken);
-	 	printf("MegaBytes per second written = %4.2lf, Blocksize:%ldk\n", mbps, writeblocksize/1024);
- 	}
-
-	close(outputfile);
-
-	return 0;
+        return 0;
 }
